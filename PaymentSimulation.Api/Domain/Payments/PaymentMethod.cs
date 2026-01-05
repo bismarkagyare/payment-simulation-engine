b55f90f2141a7342c29ee6ecbbc5f17d0b1204ed
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-05 16:06:25 +0000

    add PaymentMethod and PaymentStatus enums

---
+++ PaymentSimulation.Api/Domain/Payments/PaymentMethod.cs
@@ -0,0 +1,8 @@
+namespace PaymentSimulation.Api.Domain.Payments;
+
+public enum PaymentMethod
+{
+    Card = 0,
+    BankTransfer = 1,
+    MobileMoney = 2,
+}
---
+++ PaymentSimulation.Api/Domain/Payments/PaymentStatus.cs
@@ -0,0 +1,10 @@
+namespace PaymentSimulation.Api.Domain.Payments;
+
+public enum PaymentStatus
+{
+    Pending = 0,
+
+    Suceeded = 1,
+
+    Failed = 2,
+}
