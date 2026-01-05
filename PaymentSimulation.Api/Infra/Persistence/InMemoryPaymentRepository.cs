20801a70ef3578f7948e32eaffe0508194a4f468
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-05 17:06:57 +0000

    feat: in memory payment repo

---
+++ PaymentSimulation.Api/Infra/Persistence/InMemoryPaymentRepository.cs
@@ -0,0 +1,20 @@
+using PaymentSimulation.Api.Application.Interfaces;
+using PaymentSimulation.Api.Domain.Payments;
+
+namespace PaymentSimulation.Api.Infra.Persistence;
+
+public class InMemoryPaymentRepository : IPaymentRepository
+{
+    private readonly Dictionary<Guid, Payment> _payments = new();
+
+    public void Add(Payment payment)
+    {
+        _payments[payment.Id] = payment;
+    }
+
+    public Payment? GetById(Guid id)
+    {
+        _payments.TryGetValue(id, out var payment);
+        return payment;
+    }
+}
