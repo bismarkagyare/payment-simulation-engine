8aa841cc890f71b053ef085b17214e2601bbd647
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-07 16:28:18 +0000

    feat: add custom validation error response and idempotency middleware

--- PaymentSimulation.Api/Program.cs
+++ PaymentSimulation.Api/Program.cs
@@ -1,7 +1,9 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using PaymentSimulation.Api.Application.Interfaces;
 using PaymentSimulation.Api.Application.Payments;
 using PaymentSimulation.Api.Infra.Persistence;
+using PaymentSimulation.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,24 @@ builder.Services.AddOpenApi();
 
 builder
     .Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            //return simple validation error response
+            return new BadRequestObjectResult(
+                new
+                {
+                    error = "Invalid request",
+                    details = context
+                        .ModelState.Where(x => x.Value?.Errors.Count > 0)
+                        .ToDictionary(x => x.Key, x => x.Value?.Errors.Select(e => e.ErrorMessage)),
+                }
+            );
+        };
+    })
     .AddJsonOptions(options =>
     {
-        //serialize enums as strings instead of integers
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
@@ -30,6 +47,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseMiddleware<IdempotencyMiddleware>();
 
 app.UseHttpsRedirection();
 app.MapControllers();
