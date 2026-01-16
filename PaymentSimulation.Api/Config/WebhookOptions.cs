d66142eb1495451da73f657b502cd0a906b806f9
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-16 14:18:40 +0000

    chore: change port

--- PaymentSimulation.Api/Config/WebhookOptions.cs
+++ PaymentSimulation.Api/Config/WebhookOptions.cs
@@ -2,7 +2,7 @@ namespace PaymentSimulation.Api.Config;
 
 public class WebhookOptions
 {
-    public string Url { get; set; } = "http://localhost:7000/webhooks/payments";
+    public string Url { get; set; } = "http://localhost:8080/webhooks/payments";
 
     public int MaxRetries { get; set; } = 3;
 }
