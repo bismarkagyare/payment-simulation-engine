565c2aca40284d6832ea92791503f805d4c5fa42
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-28 20:20:37 +0000

    feat: add RabbitMQOptions class for RabbitMQ configuration

---
+++ PaymentSimulation.Api/Config/RabbitMQOptions.cs
@@ -0,0 +1,17 @@
+namespace PaymentSimulation.Api.Config;
+
+public class RabbitMQOptions
+{
+    public const string SectionName = "RabbitMQ";
+    public string HostName { get; set; } = "localhost";
+    public int Port { get; set; } = 5672;
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string VirtualHost { get; set; } = "/";
+    public string ExchangeName { get; set; } = "payments.exchange";
+    public string ProcessQueueName { get; set; } = "payment.process";
+    public string WebhookQueueName { get; set; } = "payment.webhook";
+    public string DeadLetterQueueName { get; set; } = "payment.dlq";
+    public int MaxRetryAttempts { get; set; } = 3;
+    public int InitialRetryDelayMs { get; set; } = 1000;
+}
