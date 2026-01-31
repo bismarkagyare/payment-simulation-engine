e895f324558664c7383db763ba94a05688f22095
Bismark Agyare <bismarkagyare54@gmail.com>
2026-01-31 21:28:12 +0000

    feat: implement RabbitMQTopologySetup class for RabbitMQ messaging topology

---
+++ PaymentSimulation.Api/Messaging/RabbitMQTopologySetup.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentSimulation.Api.Messaging
+{
+    public class RabbitMQTopologySetup { }
+}
