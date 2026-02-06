884c87a6d2f1197f13b51deacea00c119e846daa
Bismark Agyare <bismarkagyare54@gmail.com>
2026-02-06 14:52:41 +0000

    feat: refactor PaymentService to use IMessagePublisher for payment message publishing

--- PaymentSimulation.Api/Application/Payments/PaymentService.cs
+++ PaymentSimulation.Api/Application/Payments/PaymentService.cs
@@ -1,6 +1,8 @@
 using PaymentSimulation.Api.Application.Interfaces;
 using PaymentSimulation.Api.Domain.Payments;
 using PaymentSimulation.Api.Infra.Queue;
+using PaymentSimulation.Api.Messaging.Contracts;
+using PaymentSimulation.Api.Messaging.Interfaces;
 
 namespace PaymentSimulation.Api.Application.Payments;
 
@@ -8,12 +10,14 @@ public class PaymentService : IPaymentService
 {
     private readonly IPaymentRepository _paymentRepository;
 
-    private readonly InMemoryQueue _queue;
+    //private readonly InMemoryQueue _queue;
+    private readonly IMessagePublisher _publisher;
+    private readonly ILogger<PaymentService> _logger;
 
-    public PaymentService(IPaymentRepository paymentRepository, InMemoryQueue queue)
+    public PaymentService(IPaymentRepository paymentRepository, IMessagePublisher publisher)
     {
         _paymentRepository = paymentRepository;
-        _queue = queue;
+        _publisher = publisher;
     }
 
     public Payment CreatePayment(long amount, PaymentMethod method)
@@ -22,7 +26,24 @@ public class PaymentService : IPaymentService
 
         _paymentRepository.Add(payment);
 
-        _queue.Enqueue(payment.Id);
+        var message = new PaymentCreatedMessage
+        {
+            PaymentId = payment.Id,
+            Amount = payment.Amount,
+            Method = method.ToString(),
+            CreatedAtUtc = payment.CreatedAtUtc,
+            RetryCount = 0, // First attempt
+        };
+
+        _ = _publisher.PublishAsync("payment.created", message);
+
+        _logger.LogInformation(
+            "Payment {PaymentId} created and published for processing. "
+                + "Amount: {Amount}, Method: {Method}",
+            payment.Id,
+            payment.Amount,
+            payment.Method
+        );
 
         return payment;
     }
